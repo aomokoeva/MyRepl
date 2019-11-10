58f42642101fff1fda45fbe021f92918863220e8
aisuluuomokoeva <aomokoeva@mail.ru>
2019-11-10 14:42:46 -0600

    changed code

--- src/MethodsReturn/countAppearances.java
+++ src/MethodsReturn/countAppearances.java
@@ -2,12 +2,19 @@ package MethodsReturn;
 
 public class countAppearances {
     public static void main(String[] args) {
-        String [] arr = new String[4];
-     arr = new String[]{"first", "second", "first", "fifth"};
-        System.out.println(count_appearance(arr, "first"));
-
+        System.out.println(max(5,0));
 
     }
+
+        public static int max (int x,int  max){
+            if(max < x)
+            {
+                return max;
+            }else
+              return x;
+
+        }
+
 
     public static int count_appearance (String [] arr, String t){
         int  count = 0;
